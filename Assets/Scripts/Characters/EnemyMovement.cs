62aecd37f1513dd736113927eafcfe0a9dfa5433
Luca di Pietro <alivezn1@hotmail.com>
2023-11-15 10:35:00 +0100

    Npc_Guard Movement and Animation

--- Assets/Scripts/Characters/EnemyMovement.cs
+++ Assets/Scripts/Characters/EnemyMovement.cs
@@ -11,25 +11,16 @@ public class EnemyMovement : MonoBehaviour
 
     public float moveSpeed = 1;
 
+    public float runMultiplayer = 1.5f;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-    }
+    private float runMulti = 1;
 
     public void Movement(Vector2 endPoint, bool run)
     {
         startingPoint = transform.position;
 
-        var direction = endPoint - startingPoint;
+        var direction = (endPoint - startingPoint).normalized;
 
-        animator.SetBool("Movement", true);
-        
         animator.SetBool("isRun", run);
         animator.SetFloat("x_Input", direction.x);
         animator.SetFloat("y_Input", direction.y);
@@ -39,19 +30,23 @@ public class EnemyMovement : MonoBehaviour
         animator.SetFloat("idle_y_input", direction.y);
 
 
-        StartCoroutine(MoveRoutine(endPoint));
+        StartCoroutine(MoveRoutine(endPoint, run));
     }
 
-    IEnumerator MoveRoutine(Vector2 endPoint)
+    IEnumerator MoveRoutine(Vector2 endPoint, bool run)
     {
+        runMulti = run ? runMultiplayer : 1;
+        
+        animator.SetBool("Movement", true);
+
         while (Vector2.Distance(transform.position, endPoint) > 0.1f)
         {
-            transform.position = Vector2.MoveTowards(transform.position, endPoint, 0.016f * moveSpeed);
+            transform.position = Vector2.MoveTowards(transform.position, endPoint, 0.016f * moveSpeed * runMulti);
             yield return null;
         }
-        
+
         animator.SetBool("Movement", false);
-        
+
         animator.SetBool("isRun", false);
     }
 }
